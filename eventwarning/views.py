b43bf78ef1a7cd276acf101eaac18cb3d721dbb5
George Louthan <duplico@dupli.co>
2012-02-13 03:54:47 -0600

    Cobble together some basic web views and CSS/etc.

--- eventwarning/views.py
+++ eventwarning/views.py
@@ -1,3 +1,23 @@
 from flask import request, make_response, render_template, url_for, flash
 from flask import redirect
 import flask
+
+from eventwarning import app
+
+# /
+# /zip/<zip>/<date>
+#
+#
+
+
+@app.route('/', methods=['GET',])
+def landing():
+    pass
+
+@app.route('/zip/<zip>/d/<date>/', methods=['GET',])
+def danger_zip(zip, date):
+    #return 'danger for %s on %s' % (zip, date)
+    return render_template('base.html')
+    return render_template('scenario.html', name=name, ag=ag,
+                           nm=nm, xp=xp, owner=owner_username)
+
