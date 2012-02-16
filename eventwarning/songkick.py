6ae07c5633d8214e024b1d6ba0737ca90752daf8
George Louthan <georgerlouth@nthefourth.com>
2012-02-16 02:50:57 -0600

    Fix some songkick bugs, adding venue_search and refactoring.

--- eventwarning/songkick.py
+++ eventwarning/songkick.py
@@ -8,10 +8,16 @@ class SongkickAPI(object):
         self.api_key = api_key
         self.connection = Connection('http://api.songkick.com/api/3.0/')
 
-    def venue_search(self, query, page=1):
-        return self._get('/search/venues.json',
+    def venue_search(self, query):
+        return self._get_list('/search/venues.json',
                          params=dict(query=query, page=page),
                          resource_type='venue')
+
+    def venue_details(self, venue_id):
+        return self._get_single(
+            '/venues/%i.json' % venue_id,
+            resource_type='venue'
+        )
 
     def location_search(self, name=None, location=None):
         raise NotImplementedError
@@ -21,30 +27,41 @@ class SongkickAPI(object):
 
     def event_search(self, artist_name=None, location=None, min_date=None,
                      max_date=None):
-        assert artist_name and location and min_date and max_date
-        return self._get('/events.json', params=dict(
-            artist_name=artist_name,
-            location=location,
-            min_date=min_date,
-            max_date=max_date,
-        ), resource_type='event')
+        assert location and min_date and max_date
+        params = dict(location=location, min_date=min_date, max_date=max_date)
+        if artist_name:
+            params.update(artist_name=artist_name)
+        return self._get_list('/events.json', params=params, resource_type='event')
 
     def _get(self, resource, params={}, resource_type='event'):
+        params.update(apikey=self.api_key)
+        query_result = self.connection.request_get(resource, args=params,
+                                                   headers={'cache-control': 'no-cache'})
+        object_result = json.loads(query_result['body'])['resultsPage']
+        return object_result
+
+    def _get_list(self, resource, params={}, resource_type='event'):
         pages = 1
         current_page = 0
         result = []
 
         while current_page < pages:
-            params.update(apikey=self.api_key, page=current_page+1)
+            params.update(page=current_page+1)
             current_page+=1
-            #
-            query_result = self.connection.request_get(resource, args=params,
-                                                       headers={'cache-control': 'no-cache'})
-            object_result = json.loads(query_result['body'])['resultsPage']
-            #
+            object_result = self._get(resource, params=params,
+                                      resource_type=resource_type)
             per_page = float(object_result['perPage'])
             tot_entries = int(object_result['totalEntries'])
             pages = int(math.ceil(tot_entries / per_page))
             if object_result['results']:
                 result = result + object_result['results'][resource_type]
+        return result
+
+    def _get_single(self, resource, params={}, resource_type='event'):
+        object_result = self._get(resource, params=params,
+                                  resource_type=resource_type)
+        if object_result['results']:
+            result = object_result['results'][resource_type]
+        else:
+            result = None
         return result
